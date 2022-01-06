922112f98384f770ad4bf699fd2a2f6ac010545c
sajjad <sajjadurrahman3434@gmail.com>
2022-01-07 00:20:41 +0600

    updated

--- src/components/TableView/TableView.js
+++ src/components/TableView/TableView.js
@@ -1,19 +1,9 @@
-import React, { useState, useEffect } from "react";
-import { Button, Table } from "react-bootstrap";
-import BookProduct from '../BookProduct/Booking'
-import ReturnProduct from '../ReturnProduct/Return'
+import React from "react";
+import { Table } from "react-bootstrap";
 import "../style.css"
 
-export default function DataTable(props) {
-    const [isOpen, setIsOpen] = useState(false);
-    const [isOpenReturn, setIsOpenReturn] = useState(false);
+export default function Table(props) {
 
-    function toggleModal() {
-        setIsOpen(true);
-    }
-    function toggleModalReturn() {
-        setIsOpenReturn(true);
-    }
     return (
         <div className="App">
             <div className="">
@@ -58,14 +48,6 @@ export default function DataTable(props) {
                         </tbody>
                     </Table>
                 </div>
-            </div>
-            <div className="mb-5" style={{ marginRight: 21, display: 'flex', justifyContent: 'flex-end' }}>
-                <Button onClick={toggleModal} style={{ width: 85 }} >Book</Button>
-                <Button onClick={toggleModalReturn} style={{ marginLeft: 5, width: 85 }} variant="danger">Return</Button>
-            </div>
-            <div>
-                {isOpen ? <BookProduct status={isOpen} /> : ""}
-                {isOpenReturn ? <ReturnProduct status={isOpenReturn} /> : ""}
             </div>
         </div >
     );
